d2cb32aa1a1921128130f10aa6cc3859d92229c2
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-11-26 18:32:25 +0100

    WIP - new calendar displayed; CSS added to it.

--- js/diary.js
+++ js/diary.js
@@ -6,6 +6,20 @@ const DIARY = require("../domain/diary");
 // Here we begin to print the diary
 let htmlDiary = document.getElementById("diary");
 
+let daysInCalendar = 1;
 DIARY.forEach(day => {
-  htmlDiary.innerHTML = JSON.stringify(day);
+  let dayToAdd = document.createElement("span");
+  dayToAdd.innerHTML = daysInCalendar;
+  daysInCalendar++;
+  htmlDiary.appendChild(dayToAdd);
 });
+
+// DIARY.forEach(day => {
+//   let node = document.createElement("ul");
+//   day["eventos"].forEach(action => {
+//     let actionToAdd = document.createElement("li");
+//     actionToAdd.innerHTML = action;
+//     node.appendChild(actionToAdd);
+//   })
+//   htmlDiary.appendChild(node);
+// });
