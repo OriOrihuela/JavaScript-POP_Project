8d3dd02ea4aa036560eaf6dddfbd9e989f6c77e9
Eduardo Orihuela Verdugo <Eduardo.orihuela@sm2baleares>
2019-11-27 14:06:38 +0100

    WIP - diary.html, .js and .css with new implementations (cards and calendar).

--- js/diary.js
+++ js/diary.js
@@ -4,36 +4,67 @@
 const DIARY = require("../domain/diary");
 
 // Here we begin to print the diary
-let htmlDiary = document.getElementById("diary");
+let calendar = document.getElementById("calendar");
 
+// Here we initialize the counter for the number of days in the calendar.
 let daysInCalendar = 1;
-let index = 0;
 
-DIARY.forEach(() => {
-    let dayToAdd = document.createElement("span");
-    dayToAdd.innerHTML = daysInCalendar;
-    dayToAdd.onclick = function() {
-        getDay(this.innerHTML - 1);
-    }
-    index++;
-    daysInCalendar++;
-    htmlDiary.appendChild(dayToAdd);
+DIARY.forEach(day => {
+  // Here we create the "span" element which will contain the number of the day in the diary.
+  let dayToAdd = document.createElement("span");
+  // We acces to the stringfied value in "dayToAdd" and set it the actual value of the counter.
+  dayToAdd.innerHTML = daysInCalendar;
+  // Here we add an "onclick" event to every "span" which is being created dynamically.
+  dayToAdd.onclick = function() {
+    // Gather all the info about the current day in the Diary.
+    displayDay(day);
+    // Get all the "span" elements in the HTML.
+    var spansSelected = document.getElementsByTagName("span");
+    // Call the "removeCircleClass" function to remove the class "circle" if needed.
+    removeCircleClass(spansSelected);
+    // Once the "span" element is clicked, we add to it the class "circle".
+    dayToAdd.classList.add("circle");
+  };
+  // Increment the counter of days in the calendar.
+  daysInCalendar++;
+  // We append to the "div" element with id "calendar" the new "span" created.
+  calendar.appendChild(dayToAdd);
 });
 
-
-function getDay(day) {
-
-
-    console.log(DIARY[day]);
-
+/**
+ * This function will remove the class "circle" for every element into the array passed by parameter
+ * if the element itself has the class "circle"
+ * @param {HTMLSpanElement} spansSelected
+ */
+function removeCircleClass(spansSelected) {
+  for (let index = 0; index < spansSelected.length; index++) {
+    const element = spansSelected[index];
+    if (element.classList.contains("circle")) {
+      element.classList.remove("circle");
+    }
+  }
 }
 
-// DIARY.forEach(day => {
-//   let node = document.createElement("ul");
-//   day["eventos"].forEach(action => {
-//     let actionToAdd = document.createElement("li");
-//     actionToAdd.innerHTML = action;
-//     node.appendChild(actionToAdd);
-//   })
-//   htmlDiary.appendChild(node);
-// });
+/**
+ * 
+ * @param {JS Object} day 
+ */
+function displayDay(day) {
+  document
+    .getElementById("pop-image")
+    .setAttribute(
+      "src",
+      day.pulpo ? "/pop.45fff62b.png" : "/canitaBrava.3e228692.png"
+    );
+  document.getElementById("eventos").innerHTML = day["eventos"];
+  removeHidden(document.getElementById("card-container"));
+}
+
+/**
+ * This function checks if the element passed by parameter has the attr "hidden" set.
+ * If so, removes the attr.
+ * @param {HTMLElement} element 
+ */
+function removeHidden(element) {
+  if (element.hasAttribute("hidden")) element.removeAttribute("hidden");
+}
