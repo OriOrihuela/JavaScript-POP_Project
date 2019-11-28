8d0069e728fd7545be01278b301a7c89ce14b5a3
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-11-28 22:51:49 +0100

    WIP #10 - testing deployment changing package.json + index.js.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-// server.js
-const EXPRESS = require('express');
-const APP = EXPRESS();
-const PATH = require("path");
+// We need to import certain functionalities.
+var express = require("express");
+var app = express();
 
-APP.get('/', function(request, response) {
-    response.sendFile(PATH.join(__dirname, 'index.html'));
+/**
+ * Set the port of our application
+ * process.env.PORT lets the port be set by Heroku
+ */
+var port = process.env.PORT || 8080;
+
+// Make express look in the public directory for assets (css/js/img/html).
+app.use(express.static(__dirname + "/dist"));
+
+// Set the home page route.
+app.get("/", function(request, response) {
+  // ejs render automatically looks in the views folder
+  response.render("index");
 });
 
-APP.listen(process.env.PORT || 4000, function(){
-    console.log('Your node js server is running');
+// Just a console to check that everything is running under control.
+app.listen(port, function() {
+  console.log("Our app is running on http://localhost:" + port);
 });
